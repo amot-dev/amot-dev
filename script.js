c84a552b6d49873325a41148083e0fd9980fba62
Alexander Mot <asd777101@gmail.com>
2022-06-06 17:09:27 -0700

    Redo everything tbh

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function(){
 	var languages = document.getElementsByClassName("language");
 	for (var i = 0; i < languages.length; i++) {
-		if (languages[i].innerText == "C++") languages[i].style.borderColor = "#f34b7d";
-		if (languages[i].innerText == "VHDL") languages[i].style.borderColor = "#adb2cb";
-		if (languages[i].innerText == "HTML") languages[i].style.borderColor = "#e34c26";
-		if (languages[i].innerText == "Bash") languages[i].style.borderColor = "#89e051";
+		if (languages[i].firstChild.innerText == "C++") languages[i].style.boxShadow = "0 0 0 0.2rem #f34b7d inset";
+		if (languages[i].firstChild.innerText == "VHDL") languages[i].style.boxShadow = "0 0 0 0.2rem #adb2cb inset";
+		if (languages[i].firstChild.innerText == "HTML") languages[i].style.boxShadow = "0 0 0 0.2rem #e34c26 inset";
+		if (languages[i].firstChild.innerText == "Bash") languages[i].style.boxShadow = "0 0 0 0.2rem #89e051 inset";
 	};
 };
